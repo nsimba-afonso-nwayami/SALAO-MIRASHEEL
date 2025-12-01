806ad6ac420eee386e5f28876fdffc4040f93c51
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2025-12-01 13:40:02 +0000

    Página de total de visitas criada

---
+++ src/pages/VisitasSite.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+import "../assets/css/style.css";
+import "../assets/css/services.css";
+import "../assets/css/agendar.css";
+
+export default function VisitasSite() {
+
+    return (
+        <>
+            <title>Visitas do Site | Salão Mirashell</title>
+
+            <Header />
+
+            <section className="banner agendar">
+                <div className="content">
+                    <h3>Visitantes do site</h3>
+                    <p>Total de visitas: <strong>20.0000</strong></p>
+                </div>
+            </section>
+
+            <Footer />
+        </>
+    );
+}
