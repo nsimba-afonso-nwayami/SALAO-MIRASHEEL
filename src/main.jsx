374862a0aa515edb2e9a413ceb89cb869106b999
nsimba-afonso-nwayami <nsimba2025nwayami@gmail.com>
2025-12-04 14:55:30 +0000

    Toast adicionado de forma global na aplicação

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,20 @@ import './index.css'
 import { BrowserRouter } from "react-router-dom";
 import { HashRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HashRouter>
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        toastOptions={{
+          style: {
+            fontSize: "1.8rem",
+          }
+        }}
+      />
       <AppRoutes />
     </HashRouter>
   </StrictMode>
